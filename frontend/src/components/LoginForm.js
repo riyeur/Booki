98f04fd7e18586855bca7fe7bf63c0cd561ba823
Hooriya Khan <riyeur@hotmail.com>
2025-03-15 00:24:11 -0600

    Fixed routing and invalid username and password functionality

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -28,24 +28,28 @@ const LoginForm = () => {
         }
     };
 
+    const registerRoute = () => {
+        navigate('/register');
+    }
+
     return (
         <div className='login-form'>
             <div className='login-title-and-form'>
                 <h2 className='login-title'>Login</h2>
-                {loginFailed && <p style={{ color: 'red', textAlign: 'center' }}>{loginFailed}</p>}
                 <form className='form-section' onSubmit={handleSubmit}>
                     <input type='text' placeholder='Enter your username' required className='username' onChange={(e) => setUsername(e.target.value)}></input>
                     <input type='password' placeholder='Enter your password' required className='password' onChange={(e) => setPassword(e.target.value)}></input>
                     <input type='submit' value='Sign in' className='sign-in-button'></input>
                 </form>
+                {loginFailed && <p className='login-error'>{loginFailed}</p>}
             </div>
             <hr className='boundary'></hr>
             <div className='second-section'>
                 <p className='dont-have-account'>Don't have an account?</p>
-                <button className='register'>Register</button>
+                <button className='register' onClick={registerRoute}>Register</button>
                 <div className='continue-as-guest'>
                     <p className='continue-as'>or continue as </p>
-                    <a href='url' className='guest'>Guest</a>
+                    <a href='/llm-prompt' className='guest'>Guest</a>
                 </div>
             </div>
         </div>
