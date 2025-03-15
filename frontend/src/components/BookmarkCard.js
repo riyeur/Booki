4bf82beaa4313d3503ae0838329199b93353a09d
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-14 20:46:56 -0600

    added the functionality for bookmark card

--- frontend/src/components/BookmarkCard.js
+++ frontend/src/components/BookmarkCard.js
@@ -1,1 +1,17 @@
 // This component represents a single bookmark
+import React from 'react';
+import '../styles/ProfilePageStyles.css'
+
+const BookmarkCard = ({bookName, authorName, accessibilityInfo, bookDescription}) => {
+    return (
+        <div className='bookmark-card'>
+            <h2>{bookName}</h2>
+            <h3>Author: {authorName}</h3>
+            <p>Accessibility: {accessibilityInfo}</p>
+            <p>Description: {bookDescription}</p>
+            <button>Delete</button>
+        </div>
+    );
+};
+
+export default BookmarkCard;
