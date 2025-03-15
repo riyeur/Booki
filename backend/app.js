852d46e3ab67579de03ead7e77a3f4e5624acceb
Hooriya <121422181+riyeur@users.noreply.github.com>
2025-03-14 19:01:07 -0600

    Fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,37 @@
 import express from 'express';
 import config from './index.js';
 import loginRoute from './routes/LoginRoute.js';
+import connection from './persistence-layer/connection.js';
 import cors from 'cors';
 
 class Main {
     constructor() {
         this.app = express();
-        this.setupMiddleware();
+        this.setupExpressCors();
         this.setupRoutes();
+        this.setupDatabase();
     }
 
-    setupMiddleware() {
+    setupExpressCors() {
         this.app.use(express.json());
         this.app.use(cors());
     }
 
     setupRoutes() {
-        this.app.use('/api/auth', loginRoute);
+        this.app.use('/api/user', loginRoute);
+    }
+
+    setupDatabase() {
+        connection.connect();
+        console.log('Connected to database.')
     }
 
     start(port) {
-        this.app.listen(port, () => {
-            console.log(`Server has started on port ${port}`);
-        });
+        this.app.listen(port);
+        console.log('Server has started.')
     }
 }
 
 // Instantiate and start the backend server
 const mainApp = new Main();
 mainApp.start(config.port);
-
