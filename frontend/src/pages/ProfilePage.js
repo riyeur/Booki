9a699d73d9879de2ace934e8a9d69380d8c8dace
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-15 14:54:02 -0600

    added the sutom book button and welcome panel to the profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -3,10 +3,17 @@ import React from 'react';
 import '../components/BookmarkList';
 import '../styles/ProfilePageStyles.css';
 import BookmarkList from '../components/BookmarkList';
+import WelcomePanel from '../components/WelcomePanel';
+import BookButton from '../components/BookButton';
 
 const ProfilePage = () => {
+    const handleClick = () => {
+        alert('Change this to navigate to llm prompt page');
+      };
     return(
         <div className='profile-page'>
+            <BookButton onClick={handleClick}/>
+            <WelcomePanel></WelcomePanel>
             <BookmarkList></BookmarkList>
         </div>
     );
