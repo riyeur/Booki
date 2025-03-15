f79d8fbbe51f91114eea8772d4e62d0754ad080a
Hooriya Khan <riyeur@hotmail.com>
2025-03-15 13:48:09 -0600

    Added in JWT token functionality

--- frontend/src/pages/LLMPromptPage.js
+++ frontend/src/pages/LLMPromptPage.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import LLMPromptForm from '../components/LLMPromptForm';
 import '../styles/LLMPromptPageStyles.css'
 
 const LLMPromptPage = () => {
+    const navigate = useNavigate();
+
+    const redirectUserProfile = () => {
+        const token = sessionStorage.getItem('token');
+
+        navigate('/profile');
+
+        if (token) {
+            navigate('/profile');
+        } else {
+            navigate('/');
+        }
+    }
+
     return (
         <div className='llm-prompt-page'>
-            <div className='profile'>
+            <div className='profile' onClick={redirectUserProfile}>
                 <img className='profile-logo' src='/profile.png' alt='Profile pic' height='45' width='auto'/>
             </div>
             <LLMPromptForm></LLMPromptForm>
