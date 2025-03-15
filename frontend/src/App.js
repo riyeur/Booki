921b4bb6258da0b14c3a4098a95ae40e74c7334d
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-14 20:46:14 -0600

    added profile page to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 
 const App = () => {
   return (
       <Router>
           <Routes>
-              {/*<Route path="/" element={<LoginPage />} /> write path like this for page*/}
+              <Route path="/" element={<ProfilePage/>} /> 
           </Routes>
       </Router>
   );
