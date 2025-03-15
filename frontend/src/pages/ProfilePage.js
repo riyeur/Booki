10fb6fb7373c7b9bf5226d2284bbd96095dfd031
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-14 20:47:41 -0600

    added the bookmark list to profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,1 +1,15 @@
 // This page contains the user's profile information and their saved bookmarks
+import React from 'react';
+import '../components/BookmarkList';
+import '../styles/ProfilePageStyles.css';
+import BookmarkList from '../components/BookmarkList';
+
+const ProfilePage = () => {
+    return(
+        <div className='profile-page'>
+            <BookmarkList></BookmarkList>
+        </div>
+    );
+};
+
+export default ProfilePage;
