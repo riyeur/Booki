4101c95a56ff00edc2168d6de82487d5b59097ff
Hooriya Khan <riyeur@hotmail.com>
2025-03-16 01:43:09 -0600

    fixed some little things

--- frontend/src/pages/signupPage.js
+++ frontend/src/pages/signupPage.js
@@ -7,8 +7,8 @@ import '../styles/RegisterPageStyle.css'
 const SignupPage = () => {
     return (
         <div className='signup-page'>
-            <div className='components'>
-                <img className='booki-logo' src='/booki_logo.png' alt='Booki Logo' height='105' width='330'/>
+            <div className='register-components'>
+                <img className='register-booki-logo' src='/booki_logo.png' alt='Booki Logo' height='105' width='auto'/>
                 <RegisterForm></RegisterForm>
             </div>
         </div>
