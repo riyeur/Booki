87ff16dc20da99401b1d313e851dd1056600d41c
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-16 16:26:22 -0600

    added results page path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LLMPromptPage from './pages/LLMPromptPage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import SignupPage from './pages/signupPage';
+import ResultsPage from './pages/ResultsPage';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
               <Route path="/signup" element={<SignupPage />} />
               <Route path="/profile" element={<ProfilePage />} />
               <Route path="/llm-prompt" element={<LLMPromptPage />} />
+              <Route path="llm-results" element={<ResultsPage />} />
           </Routes>
       </Router>
   );
