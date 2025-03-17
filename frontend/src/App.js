aefc419682e6ee4efe23b342d0d5b82b76811863
Hooriya Khan <riyeur@hotmail.com>
2025-03-17 08:58:28 -0600

    Fixed profile route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LLMPromptPage from './pages/LLMPromptPage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
+import ProtectedRoute from './pages/ProtectedRoute';
 import SignupPage from './pages/signupPage';
 import ResultsPage from './pages/ResultsPage';
 
@@ -12,7 +13,9 @@ const App = () => {
           <Routes>
               <Route path="/" element={<LoginPage />} />
               <Route path="/signup" element={<SignupPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
+              <Route element={<ProtectedRoute/>}>
+                <Route path="/profile" element={<ProfilePage />} />
+              </Route>
               <Route path="/llm-prompt" element={<LLMPromptPage />} />
               <Route path="/llm-results" element={<ResultsPage />} />
           </Routes>
---
+++ frontend/src/pages/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+    const token = sessionStorage.getItem('token');
+
+    if (!token) {
+        return <Navigate to="/" />;
+    }
+
+    return <Outlet />;
+}
+
+export default ProtectedRoute;
