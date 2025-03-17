805f9a80dba4cbbe11fe2b4cd14304c0836f590d
Hooriya Khan <riyeur@hotmail.com>
2025-03-17 13:35:58 -0600

    FIxed connection

--- backend/persistence-layer/connection.js
+++ backend/persistence-layer/connection.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log("DATABASE_URL:", process.env.CONNECTION_URL);
-
 const connection = await mysql.createConnection(process.env.CONNECTION_URL);
 
 export default connection;
