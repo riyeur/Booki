22cd5194c2e8ab13f0ca3121cbebdc7fc676754a
Hooriya Khan <riyeur@hotmail.com>
2025-03-17 22:47:26 -0600

    The deployed website works!!!!!

--- backend/business-layer/services/LoginService.js
+++ backend/business-layer/services/LoginService.js
@@ -12,38 +12,29 @@ class LoginService {
     }
 
     async authenticateUser(username, password) {
-        console.log("Reached service");
         // Get user from the database (calls the persistence layer)
         const bookiUser = await this.users.getUserByUsername(username);
 
         if (!bookiUser) {
-            console.log("User not found.");
             return false;
         }
-
-        console.log("Returned service");
 
         if (password != bookiUser.User_Password) {
             return false;
         }
 
-        console.log("Creating JWT token...");
-
         // Get the JWT token
         const token = this.getJWTToken(bookiUser);
-
-        console.log("Returning JWT token...");
 
         return token;
     }
 
     getJWTToken(user) {
 
-        console.log("Creating JWT token:", user);
         const token = jwt.sign(
             { userID: user.User_ID, username: user.Username }, process.env.JWT_SECRET, { expiresIn: '1h'}
         );
-        console.log("JWT created:", token);
+
         return token;
     }
 }
