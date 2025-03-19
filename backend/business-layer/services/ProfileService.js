1786354c7b78f3a635eccf7606cc6ba612382965
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-19 16:14:31 -0600

    moved into try catch, decoded token, removed log

--- backend/business-layer/services/ProfileService.js
+++ backend/business-layer/services/ProfileService.js
@@ -1,4 +1,5 @@
 import Bookmarks from '../../persistence-layer/database-functions/Bookmarks.js';
+import jwt from 'jsonwebtoken';
 
 class ProfileService {
     constructor(bookmarks) {
@@ -6,19 +7,21 @@ class ProfileService {
         this.getUserBookmarksById = this.getUserBookmarksById.bind(this);
     }
 
-    async getUserBookmarksById(userId) {
-        // check that the current user is authorized/ has an Id
-        if(userId == null){
-            return false;
-        }
-
+    async getUserBookmarksById(token) {
         try{
+            // decode the token and extract the user id from it
+            const decoded_token = jwt.verify(token, process.env.JWT_SECRET);
+            const userId = decoded_token.userID;
+
+            // check that the current user is authorized/ has an Id
+            if(!userId){
+                return null;
+            }
             // get the user's bookmarks using the sql querry in the persistence layer
             const userBookmarks = await this.bookmarks.getBooksByUserId(userId);
             return userBookmarks;
         }
         catch (error){
-            console.log("error in profile service.js:", error);
             return null;
         }
 
