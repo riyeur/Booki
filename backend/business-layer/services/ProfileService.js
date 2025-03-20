63da69ec7a36717abcc9a34c76cefb9090738907
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2025-03-20 00:12:43 -0600

    added getUsernameById function, made extracting id modular with new function, added users to constructor

--- backend/business-layer/services/ProfileService.js
+++ backend/business-layer/services/ProfileService.js
@@ -1,17 +1,25 @@
 import Bookmarks from '../../persistence-layer/database-functions/Bookmarks.js';
+import Users from '../../persistence-layer/database-functions/Users.js';
 import jwt from 'jsonwebtoken';
 
 class ProfileService {
-    constructor(bookmarks) {
+    constructor(bookmarks, users) {
         this.bookmarks = bookmarks;
+        this.users = users;
         this.getUserBookmarksById = this.getUserBookmarksById.bind(this);
+        this.getUsernameById = this.getUsernameById.bind(this);
+    }
+
+    extractUserId(token){
+        // decode the token and extract the user id from it
+        const decoded_token = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded_token.userID;
+        return userId;
     }
 
     async getUserBookmarksById(token) {
         try{
-            // decode the token and extract the user id from it
-            const decoded_token = jwt.verify(token, process.env.JWT_SECRET);
-            const userId = decoded_token.userID;
+            const userId = this.extractUserId(token);
 
             // check that the current user is authorized/ has an Id
             if(!userId){
@@ -26,6 +34,23 @@ class ProfileService {
         }
 
     }
+
+    async getUsernameById(token) {
+        try{
+            const userId = this.extractUserId(token);
+
+            // check that the current user is authorized/ has an Id
+            if(!userId){
+                return null;
+            }
+            // get the user's username using the sql querry in the persistence layer
+            const username = await this.users.getUsernameByUserId(userId);
+            return username;
+        }
+        catch (error){
+            return null;
+        }
+    }
 }
 
-export default new ProfileService(Bookmarks);
+export default new ProfileService(Bookmarks, Users);
